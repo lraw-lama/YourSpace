ca9785f7a627ffe937027df2c07111f3fcb6c80c
La Raw <lr5469@rit.edu>
2018-10-16 15:07:57 -0400

    HomeScreen

---
+++ app/src/main/java/com/example/laraw/yourspace/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.laraw.yourspace
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+class MainActivity : AppCompatActivity() {
+    //Button button;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Button button = (Button) findView
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/laraw/yourspace/SignInPage.kt
@@ -0,0 +1,12 @@
+package com.example.laraw.yourspace
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_page)
+    }
+}
